303f75533aec46156c56f4bf7df19250b9517758
J Kangas <jussi.v.kangas@gmail.com>
2020-09-24 17:16:56 +0200

    Trajectory funkar

---
+++ Scripts/StellarSystemSimulations/StellarBody.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StellarBody : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public bool staticBody = false;
+    
+    public static List<StellarBody> StellarBodies;
+    
+    void Awake(){
+        if (staticBody)
+           rigidBody.isKinematic = true;
+            
+    }
+
+    // For adding StellarBodys to the StellarBodys list
+    void OnEnable(){  
+        if(StellarBodies == null){
+            StellarBodies = new List<StellarBody>();
+        }
+        StellarBodies.Add(this);
+    }
+
+    // For removing StellarBodys from the StellarBodys list
+    void OnDisable(){
+        StellarBodies.Remove(this);
+    }
+
+}
