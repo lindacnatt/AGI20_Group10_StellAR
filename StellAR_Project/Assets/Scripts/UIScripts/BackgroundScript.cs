1c77f859eb1e2b463d0e29198627a999539febe9
lindacnatt <linda.cnattingius@gmail.com>
2020-09-27 18:11:48 +0200

    add test elements, animated background and 2 screens
    no functionality yet. Still investigating

---
+++ StellAR_Project/Assets/Scripts/UIScripts/BackgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+    
+        Material mat = mr.material; //first copy
+
+        Vector2 offset = mat.GetTextureOffset("_MainTex");
+
+        offset.x += Time.deltaTime / 80f;
+        offset.y += Time.deltaTime / 75f;
+
+
+        mat.SetTextureOffset("_MainTex", offset);
+    }
+}
