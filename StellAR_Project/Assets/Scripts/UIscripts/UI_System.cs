f0f5f5f441da3eca45769306e107d46b093a7fee
lindacnatt <linda.cnattingius@gmail.com>
2020-09-29 20:16:35 +0200

    add the logic of system events

--- StellAR_Project/Assets/Scripts/UIscripts/UI_System.cs
+++ StellAR_Project/Assets/Scripts/UIscripts/UI_System.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Stellar.UI
 {
@@ -9,8 +10,19 @@ namespace Stellar.UI
     {
 
         #region Variables
-        public Component[] screens = new Component[0];
+        [Header("system Events")]
+        public UnityEvent onSwitchedScreen = new UnityEvent();
+
+        private Component[] screens = new Component[0];
+
+        private UI_Screen previousScreen;
+        public UI_Screen PreviousScreen{get{return previousScreen;}}
+
+        private UI_Screen currentScreen;
+        public UI_Screen CurrentScreen{get{return currentScreen;}}
+        
         #endregion
+        
 
         #region Main Methods
         // Start is called before the first frame update
@@ -18,16 +30,42 @@ namespace Stellar.UI
         {
             screens = GetComponentsInChildren<UI_Screen>(true);
         }
-    
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
         #endregion
 
         #region Helper Methods
+        public void SwitchScreens(UI_Screen aScreen)
+        {
+            if(aScreen)
+            {
+                if(currentScreen)
+                {
+                    //currentScreen.Close();
+                    previousScreen = currentScreen;
+                }
+
+                currentScreen = aScreen;
+                currentScreen.gameObject.SetActive(true);
+                //currentSreen.StartScreen();
+                if(onSwitchedScreen !=null) {
+                    onSwitchedScreen.Invoke();
+                }
+
+            }
+        }
+        public void GoToPreviousScreen(){
+            if(previousScreen){
+                SwitchScreens(previousScreen);
+            }
+        }
+        public void LoadScene(int sceneIndex)
+        {
+            StartCoroutine(WaitToLoadScene(sceneIndex));
+
+        }
+
+        IEnumerator WaitToLoadScene(int sceneIndex){
+            yield return null;
+        }
         #endregion
     }
 }
