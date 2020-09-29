56c91735361fe0347df8d0ec1216cf983c313a9d
lindacnatt <linda.cnattingius@gmail.com>
2020-09-29 19:38:02 +0200

    Set up UI handler

---
+++ StellAR_Project/Assets/Scripts/UIscripts/UI_Screen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stellar.UI
+{
+    
+
+    public class UI_Screen : MonoBehaviour
+    {
+
+        #region Variables
+        #endregion
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+    
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
---
+++ StellAR_Project/Assets/Scripts/UIscripts/UI_System.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Stellar.UI
+{
+    
+    public class UI_System : MonoBehaviour
+    {
+
+        #region Variables
+        public Component[] screens = new Component[0];
+        #endregion
+
+        #region Main Methods
+        // Start is called before the first frame update
+        void Start()
+        {
+            screens = GetComponentsInChildren<UI_Screen>(true);
+        }
+    
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        #endregion
+
+        #region Helper Methods
+        #endregion
+    }
+}
