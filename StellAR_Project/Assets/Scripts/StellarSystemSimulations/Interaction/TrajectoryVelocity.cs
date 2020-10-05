25f562ecf5f3008de7a976a7b5bf7d4eacd550f8
J Kangas <jussi.v.kangas@gmail.com>
2020-10-05 15:06:44 +0200

    Trajectory interaction

---
+++ StellAR_Project/Assets/Scripts/StellarSystemSimulations/Interaction/TrajectoryVelocity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrajectoryVelocity : MonoBehaviour
+{
+
+    public GameObject mainObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SimulationPauseControl.gameIsPaused){}
+        
+    }
+}
