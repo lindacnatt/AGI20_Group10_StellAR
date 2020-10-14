2113dac5b1c48621e76cd7e9fb270ae10deb87f8
lindacnatt <linda.cnattingius@gmail.com>
2020-10-14 17:00:06 +0200

    connect triangle with heron
    raycast to collider

--- StellAR_Project/Assets/Scripts/UIscripts/Triangle.cs
+++ StellAR_Project/Assets/Scripts/UIscripts/Triangle.cs
@@ -9,10 +9,18 @@ public class Triangle : MonoBehaviour
     MeshRenderer meshRenderer;
     Vector3[] vertices;
     int[] triangles;
+    Ray ray;
+    RaycastHit2D hit;
+    Transform selection;
+    Renderer selectionRenderer;
 
     public Material material;
+
+    [Range(1,10)]
+    public float size;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         gameObject.AddComponent<MeshFilter>();
         meshRenderer = gameObject.AddComponent<MeshRenderer>();
@@ -24,8 +32,8 @@ public class Triangle : MonoBehaviour
 
         vertices = new[] {
             new Vector3(0,0,0),
-            new Vector3(0.5f,0.866025404f,0),
-            new Vector3(1,0,0)
+            new Vector3(0.5f,0.866025404f,0)*size,
+            new Vector3(1,0,0)*size
         };
         mesh.vertices = vertices;
 
@@ -33,15 +41,19 @@ public class Triangle : MonoBehaviour
 
         mesh.triangles = triangles;
 
-
-
-
-
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics2D.Raycast(ray.origin, ray.direction)){
+            Debug.Log("Hej");
+            selection = hit.transform;
+            //selectionRenderer = selection.GetComponent<Renderer>();
+            if(Input.GetMouseButtonDown(0)){
+                //Debug.Log(BaryCentric.getWeights(selection.InverseTransformPoint(hit.point), vertices));
+            }
+        }
     }
 }
