538d4c179ed305e1443875ce32c6ca7f5fca33e9
carzetdesktop <carl.a.zettergren@gmail.com>
2020-10-30 17:04:24 +0100

    Started on materialpropertyblock for instanstiation

---
+++ StellAR_Project/Assets/Shaders/GasPlanetShaderMAterialPropertyBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasPlanetShaderMAterialPropertyBlock : MonoBehaviour {
+
+    [SerializeField] Color MyColor = new Color();
+    private MaterialPropertyBlock myMatBlock = new MaterialPropertyBlock();
+    private MeshRenderer myMeshRenderer = new MeshRenderer();
+    void Start()
+    {
+        myMeshRenderer = GetComponent<MeshRenderer>();
+
+    }
+    private void Update()
+    {
+        myMatBlock.SetColor("BandColorStorm", MyColor);
+        myMeshRenderer.SetPropertyBlock(myMatBlock);
+    }
+}
