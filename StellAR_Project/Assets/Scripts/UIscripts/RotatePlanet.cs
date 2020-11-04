8244a290283864108905fe71ad8ff6838a949a62
lindacnatt <linda.cnattingius@gmail.com>
2020-11-04 14:12:15 +0100

    Made it possible to rotate planet
    needs testing on mobile

---
+++ StellAR_Project/Assets/Scripts/UIscripts/RotatePlanet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 _startingPos;
+    private Quaternion rotationY;
+
+    private float rotateSpeedMod = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedMod, 0f);
+                transform.rotation = rotationY * transform.rotation;
+            }
+        }
+        
+    }
+}
