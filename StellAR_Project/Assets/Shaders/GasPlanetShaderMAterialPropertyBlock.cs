92ab6b69718c18e90f57a2a163c8a15e9134c727
carzetdesktop <carl.a.zettergren@gmail.com>
2020-11-04 13:41:11 +0100

    Material property block basics

--- StellAR_Project/Assets/Shaders/GasPlanetShaderMAterialPropertyBlock.cs
+++ StellAR_Project/Assets/Shaders/GasPlanetShaderMAterialPropertyBlock.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class GasPlanetShaderMAterialPropertyBlock : MonoBehaviour {
 
-    [SerializeField] Color MyColor = new Color();
-    private MaterialPropertyBlock myMatBlock = new MaterialPropertyBlock();
-    private MeshRenderer myMeshRenderer = new MeshRenderer();
-    void Start()
-    {
-        myMeshRenderer = GetComponent<MeshRenderer>();
+    //The color of the object
+    public Color MaterialColor;
+    public float StormSize;
 
-    }
-    private void Update()
+    //The material property block we pass to the GPU
+    private MaterialPropertyBlock propertyBlock;
+
+    // OnValidate is called in the editor after the component is edited
+    void OnValidate()
     {
-        myMatBlock.SetColor("BandColorStorm", MyColor);
-        myMeshRenderer.SetPropertyBlock(myMatBlock);
+        //create propertyblock only if none exists
+        if (propertyBlock == null)
+            propertyBlock = new MaterialPropertyBlock();
+        //Get a renderer component either of the own gameobject or of a child
+        Renderer renderer = GetComponentInChildren<Renderer>();
+        //set the color property
+        propertyBlock.SetColor("BandColorStorm", MaterialColor);
+        propertyBlock.SetFloat("_StormSize", StormSize);
+        //apply propertyBlock to renderer
+        renderer.SetPropertyBlock(propertyBlock);
     }
-}
+} 
