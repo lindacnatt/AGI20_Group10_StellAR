4fe0e5dca75efcafa5d66f7a892099daf2ca6a82
lindacnatt <linda.cnattingius@gmail.com>
2020-11-11 11:13:09 +0100

    fix changing color of shaders tint with slider

---
+++ StellAR_Project/Assets/ChangeTint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTint : MonoBehaviour
+{
+
+     Color newTint;
+     MotherPlanet planet;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = gameObject.GetComponent<MotherPlanet>();
+
+    }
+
+
+    public void ChangeTintCol(float value){ 
+
+        planet.colorGenerator.settings.biomeColorSettings.biomes[0].tint.r = value;
+        planet.GenerateColors(); 
+
+    }
+
+}
