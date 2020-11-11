93d3c89fcdaf7d3dc77a644cbbb23ce713d17c9c
carzetdesktop <carl.a.zettergren@gmail.com>
2020-11-11 12:32:32 +0100

    sizescene

---
+++ StellAR_Project/Assets/Scripts/UIscripts/TextFromSliderValue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFromSliderValue : MonoBehaviour
+{
+    Text valueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        valueText = GetComponent<Text>(); //Gets the text part of the textobject this script is applied to
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateText (float value) //Called from the eventhandler of the slider-object
+    {
+        valueText.text = (Mathf.Round(value * 100) / 100).ToString("f2");
+    }
+}
---
+++ StellAR_Project/Assets/SizeChanger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChanger : MonoBehaviour
+{
+    public GameObject Planet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Planet == null)
+            Planet = GameObject.FindGameObjectWithTag("Planet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SizeUpdate(float value) { 
+        if (Planet.GetComponent<IsTypeOfPlanet>().IsGassy == true)
+        {
+            Debug.Log(value);
+            Planet.transform.localScale = new Vector3(value/2, value/2, value/2); //localscale adjusts diameter, to keep consistency with rocky icospheres we halve it to get a radius
+        }
+        if (Planet.GetComponent<IsTypeOfPlanet>().IsRocky == true)
+        {
+            Planet.GetComponent<IcoPlanet>().shapeSettings.radius = value;
+            Planet.GetComponent<IcoPlanet>().UpdateMesh();
+
+            Debug.Log(Planet.GetComponent<IcoPlanet>().shapeSettings.radius);
+
+            
+        }
+    }
+}
