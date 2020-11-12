ff6b3d1e2e4b1ab06f28b4ad813de0183a6f12ec
Jesper Englund <jesper.englund92@gmail.com>
2020-11-12 16:27:30 +0100

    rigid body mass

--- StellAR_Project/Assets/Scripts/StellarSystemSimulations/acceleration based gravity/CelestialObject.cs
+++ StellAR_Project/Assets/Scripts/StellarSystemSimulations/acceleration based gravity/CelestialObject.cs
@@ -22,6 +22,7 @@ public class CelestialObject : MonoBehaviour
 
     public GameObject explosionEffect;
     bool hasExploded = false;
+    public float weightMultiplier = 2;
 
 
 
@@ -30,9 +31,21 @@ public class CelestialObject : MonoBehaviour
 
     // Update call for the attractor. Runs through the static attractors list.
     void Start(){
-        rigidBody = GetComponent<Rigidbody>();
+        if (rigidBody == null)
+        {
+            rigidBody = this.gameObject.AddComponent<Rigidbody>();
+        }
+        else
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+        rigidBody.mass = this.gameObject.transform.localScale.x *
+            this.GetComponent<SphereCollider>().radius * weightMultiplier;
+        rigidBody.useGravity = false;
         if (staticBody)
+        {
             rigidBody.isKinematic = true;
+        }
 
     }
 
@@ -98,7 +111,5 @@ public class CelestialObject : MonoBehaviour
 
         Destroy(gameObject);
     }
-
-
 
 }
