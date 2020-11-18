2077724bb46c16829ebf42482e76585b1d6a3515
Jesper Englund <jesper.englund92@gmail.com>
2020-11-18 14:32:27 +0100

    more sounds

--- StellAR_Project/Assets/Scripts/PlanetCreation/Interaction/MouseInteraction.cs
+++ StellAR_Project/Assets/Scripts/PlanetCreation/Interaction/MouseInteraction.cs
@@ -28,36 +28,66 @@ public class MouseInteraction : MonoBehaviour{
     Color untoggledTxtColor = new Color(0.5f, 0.5f, 0.5f);
     Color toggledColor = new Color(0.1f, 0.0f, 0.7f);
     Color untoggledColor = new Color(0.2f, 0.2f, 0.5f);
+    bool paintAudioPlaying;
+    AudioSource paSource;
 
     void Start(){
         planet = gameObject.GetComponent<MotherPlanet>();
         timeToGo = Time.fixedTime + 0.1f;
         craterPlacement = false;
-}
+        paintAudioPlaying = false;
+        GameObject paGO = GameObject.Find("PaintPlanetAudio");
+        if (paGO == null)
+        {
+            addSound();
+        }
+        else
+        {
+            paSource = paGO.GetComponent<AudioSource>();
+        }
+    }
 
     void Update(){
         if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.C))
         {
             craterPlacement ^= true;
         }
+        paintAudioPlaying = false;
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit)){
             selection = hit.transform;
             if(craterPlacement){
                 if(Input.GetMouseButton(0)){
+                    paintAudioPlaying = true;
+                    if (!paSource.isPlaying)
+                    {
+                        paSource.Play(0);
+                    }
                     planet.shapeGenerator.craterGenerator.PlaceCrater(selection.InverseTransformPoint(hit.point));
                     planet.UpdateMesh();
                 }
             }
             else{
                 if(Input.GetMouseButton(0)){
+                    paintAudioPlaying = true;
+                    if (!paSource.isPlaying)
+                    {
+                        paSource.Play(0);
+                    }
                     interactionPoint = selection.InverseTransformPoint(hit.point);
                     hitCoords.Add(interactionPoint);
                     planet.UpdateMesh();
                     //hitCoords.Add(selection.InverseTransformPoint(hit.point));
                 }
             }   
-        }    
+        }
+        if (!paintAudioPlaying)
+        {
+            if (paSource)
+            {
+                paSource.Pause();
+            }
+        }
     }
 
     public void craterMode()
@@ -76,5 +106,26 @@ public class MouseInteraction : MonoBehaviour{
             craterBtnTxt.color = untoggledTxtColor;
         }
 
+    }
+
+    void addSound()
+    {
+        GameObject paGO = new GameObject("PaintPlanetAudio");
+        paGO.AddComponent<AudioSource>();
+        paSource = paGO.GetComponent<AudioSource>();
+        paSource.clip = Resources.Load<AudioClip>("Audio/FX-Ambient/Spaceship Engine Light");
+        paSource.volume = 0.75f;
+        paSource.loop = true;
+        paSource.playOnAwake = false;
+        paSource.pitch = 0.5f;
+        paGO.AddComponent<AudioHighPassFilter>();
+        AudioHighPassFilter highPass = paGO.GetComponent<AudioHighPassFilter>();
+        highPass.cutoffFrequency = 10;
+        paGO.AddComponent<AudioLowPassFilter>();
+        AudioLowPassFilter lowPass = paGO.GetComponent<AudioLowPassFilter>();
+        lowPass.cutoffFrequency = 1500;
+        paGO.AddComponent<AudioDistortionFilter>();
+        AudioDistortionFilter dist = paGO.GetComponent<AudioDistortionFilter>();
+        dist.distortionLevel = 0.85f;
     }
 }
