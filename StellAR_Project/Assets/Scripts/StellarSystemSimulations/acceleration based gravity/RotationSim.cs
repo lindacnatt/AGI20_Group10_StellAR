d87bdac18917d6c35e0da6067b8625ff9ca37d5c
J Kangas <jussi.v.kangas@gmail.com>
2020-11-26 10:03:06 +0100

    Tweaking rotation - not working

--- StellAR_Project/Assets/Scripts/StellarSystemSimulations/acceleration based gravity/RotationSim.cs
+++ StellAR_Project/Assets/Scripts/StellarSystemSimulations/acceleration based gravity/RotationSim.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RotationSim : MonoBehaviour
 {
     private Quaternion rotation;
+    private Vector3 rotationVec;
+    private float startZ;
     private float rotSpeed = 30f;
     private bool rotationIsSet = false;
     private bool startRot = false;
@@ -13,6 +15,9 @@ public class RotationSim : MonoBehaviour
     private Vector2 direction;
 
     // Start is called before the first frame update
+    void Start(){
+        startZ = this.gameObject.transform.rotation.eulerAngles.z;
+    }
 
     void Update(){
         if(!rotationIsSet){
@@ -23,21 +28,52 @@ public class RotationSim : MonoBehaviour
             }
             if(touch.phase == TouchPhase.Moved){
                 endPos = touch.position;
+                direction = endPos - startPos;
+                if(direction.magnitude>0.05f){
+                    float xDegree= Mathf.Rad2Deg*(Mathf.Acos(direction.x/direction.magnitude));
+                    float yDegree= Mathf.Rad2Deg*(Mathf.Asin(direction.y/direction.magnitude));
+                    rotationVec = new Vector3(xDegree, yDegree, 0f);
+                }
+
+                else{
+                    rotationVec = new Vector3(0f, 0f, 0f);
+                }
+
+                //rotation = Quaternion.Euler(xDegree, yDegree, 0f);
+                //Quaternion rotationX = Quaternion.Euler(, 0f, 0f);
+                //rotation = rotationY * rotationX;
+                startRot = true;
                 
             }
             if(touch.phase == TouchPhase.Ended){
+                endPos = touch.position;
                 direction = endPos - startPos;
-                Quaternion rotationY = Quaternion.Euler(0f, -direction.y * direction.magnitude, 0f);
-                Quaternion rotationX = Quaternion.Euler(-direction.x * direction.magnitude, 0f, 0f);
+                if(direction.magnitude>0.1f){
+                    float xDegree= Mathf.Rad2Deg*(Mathf.Acos(direction.x/direction.magnitude));
+                    float yDegree= Mathf.Rad2Deg*(Mathf.Asin(direction.y/direction.magnitude));
+                    rotationVec = new Vector3(xDegree, yDegree, 0f);
+                }
+
+                else{
+                    rotationVec = new Vector3(0f, 0f, 0f);
+                }
+                
+            }
+            /*if (touch.phase == TouchPhase.Moved)
+            {
+                
+                Quaternion rotationY = Quaternion.Euler(0f, touch.deltaPosition.y*touch.deltaPosition.magnitude , 0f);
+                Quaternion rotationX = Quaternion.Euler(touch.deltaPosition.x*touch.deltaPosition.magnitude, 0f, 0f);
                 rotation = rotationY * rotationX;
                 startRot = true;
-            }
+            }*/
         }
     }
 
     void FixedUpdate(){
         if (startRot){
-            this.gameObject.transform.Rotate(rotation.eulerAngles*Time.fixedDeltaTime);
+            //this.gameObject.transform.Rotate(rotation.eulerAngles*Time.fixedDeltaTime);
+            this.gameObject.transform.Rotate(rotationVec*Time.fixedDeltaTime);
         }
     }
 
