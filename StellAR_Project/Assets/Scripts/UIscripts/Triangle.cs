38808bb63f93113fd4bdb4bf4fa46e06a776facc
carzetdesktop <carl.a.zettergren@gmail.com>
2020-11-29 14:48:02 +0100

    Triangle works with gasplanet now :)

--- StellAR_Project/Assets/Scripts/UIscripts/Triangle.cs
+++ StellAR_Project/Assets/Scripts/UIscripts/Triangle.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,11 +21,9 @@ public class Triangle : MonoBehaviour{
     Vector2 worldPos;
     GasPlanetShaderMAterialPropertyBlock gasPlanet;
     List<Button> bandButtons = new List<Button>();
-    int currBand;
-    Vector3 band1Weights = new Vector3();
-    Vector3 band2Weights = new Vector3();
-    Vector3 band3Weights = new Vector3();
-    Vector3 stormWeights = new Vector3();
+    int currBand = 0;
+    Vector3[] bandWeights = new Vector3[4];
+    Vector3[] bandPos = new Vector3[4];
     MotherPlanet planet;
     
 
@@ -85,7 +83,7 @@ public class Triangle : MonoBehaviour{
         // set handle sprite
         handle = this.transform.GetChild(0).gameObject;
         handleInitZ = handle.transform.localPosition.z;
-        
+        Array.ForEach(bandPos, p => p = handle.transform.localPosition);
         // create shape of collider 
         colPoints = new Vector2[3];
         for(int i = 0; i < 3; i++){
@@ -96,12 +94,19 @@ public class Triangle : MonoBehaviour{
 
         //get initialweights
         Vector3 initWeights = BaryCentric.getWeights(handle.transform.position, vertices);
-        band1Weights = band2Weights = band3Weights = stormWeights = initWeights;
+        Array.ForEach(bandWeights, w => w = initWeights);
     }
   
     void Update(){
-        
+
+        if (gasPlanet)
+        {
+            //makes sure that each band keeps the color that was selected before switching to other band, and that the handle reflects that color
+            handle.transform.localPosition = new Vector3(bandPos[currBand].x, bandPos[currBand].y, handleInitZ);
+            UpdateColor(bandWeights[currBand]);
+        }
         if(Input.GetMouseButton(0)){
+
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             //worldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)) * -1f;
             hit = Physics2D.GetRayIntersection(ray);
@@ -111,9 +116,9 @@ public class Triangle : MonoBehaviour{
                 selection = hit.transform;
                 //selectionRenderer = selection.GetComponent<Renderer>();
                 handle.transform.position = hit.point;
-                Debug.Log("updated transform:"+handle.transform.position);
                 Vector3 colorWeights = BaryCentric.getWeights(handle.transform.position, vertices);
                 UpdateTintColor(colorWeights);
+                //the following line makes sure that the handle does not fuck off into oblivion on the z axis.
                 handle.transform.localPosition = new Vector3(handle.transform.localPosition.x, handle.transform.localPosition.y, handleInitZ);
                 UpdateColor(colorWeights);
             }
@@ -139,9 +144,9 @@ public class Triangle : MonoBehaviour{
         }
         else
         {
+            //makes sure that each band keeps the color that was selected before switching to other band, and that the handle reflects that color
             if (currBand == 0)
             {
-
                 gasPlanet.ChangeBandColor1(weights);
             }
             else if (currBand == 1)
@@ -156,6 +161,8 @@ public class Triangle : MonoBehaviour{
             {
                 gasPlanet.ChangeStormColor(weights);
             }
+            bandPos[currBand] = handle.transform.localPosition;
+            bandWeights[currBand] = weights;
         }
         
     }
