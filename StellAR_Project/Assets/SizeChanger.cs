c81319bdaef444113731c9fb20d626d5a5f4a356
carzetdesktop <carl.a.zettergren@gmail.com>
2020-12-15 10:55:10 +0100

    Added helptoggle and Adjusted sizechanger script a bit

--- StellAR_Project/Assets/SizeChanger.cs
+++ StellAR_Project/Assets/SizeChanger.cs
@@ -16,52 +16,46 @@ public class SizeChanger : MonoBehaviour
     private void OnDisable()
     {
         Planet = null;
-        Gas = false;
+        SizeSlider.value = 0;
+       
     }
     void OnEnable()
     {
-        
-        if (Planet == null)
+
+        if (GameObject.FindGameObjectWithTag("Planet"))
         {
+            Debug.Log(true);
             Planet = GameObject.FindGameObjectWithTag("Planet");
-            
-            if(Planet == null)
-            {
-                Planet = GameObject.FindGameObjectWithTag("GasPlanet");
-                Gas = true;
-            }
-        }
-        if (Gas)
-        {
-            //gasScaler = 1 / (Mathf.Log(11.2f * 100) / 5f);
-           
-            SizeSlider.value = 11.20f; //Defaults to Jupiter
-            SizeSlider.minValue = 2.50f;
-            SizeSlider.maxValue = 14.00f; //Jupiter is 11.2, 95% of all exo planets nasa has confirmed has a radius lower than 13.25
-            SizeUpdate(11.20f);
-        }
-        else 
-        {
-            // scale the planet to fit in screen 
-            //solidScaler = (5*Planet.GetComponent<IcoPlanet>().shapeSettings.radius) / Mathf.Log(100);
-            
-            // set default sliderValues
+            Gas = false;
             SizeSlider.value = 1.00f; //Defaults to Earth
             SizeSlider.minValue = 0.3f; //Slightly smaller than Mercury
             SizeSlider.maxValue = 2.50f; //Super-terrans
             SizeUpdate(SizeSlider.value);
         }
+
+        else if (GameObject.FindGameObjectWithTag("GasPlanet"))
+        {
+            Planet = GameObject.FindGameObjectWithTag("GasPlanet");
+            Gas = true;
+            SizeSlider.value = 11.20f; //Defaults to Jupiter
+            SizeSlider.minValue = 2.50f;
+            SizeSlider.maxValue = 14.00f; //Jupiter is 11.2, 95% of all exo planets nasa has confirmed has a radius lower than 13.25
+            SizeUpdate(SizeSlider.value);
+        }
+        
+     
     }
+   
 
     public void SizeUpdate(float value) {
         value = Mathf.Log(value*100)/5;
-        if (Gas)
+        if (Gas && Planet)
         {
             value *= scaler/* * 0.5f*/;
             Planet.transform.localScale = new Vector3(value, value, value); //localscale adjusts diameter, to keep consistency with rocky icospheres we halve it to get a radius
             
         }
-        else
+        else if(!Gas && Planet)
         {
             IcoPlanet ico = Planet.GetComponent<IcoPlanet>();
             ico.shapeSettings.radius = value/2 * scaler;
